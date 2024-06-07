096b2ddaf0760711d274f7c61f01906b42b05806
rwotter <renata.wotter@gmail.com>
2024-06-07 17:04:54 -0300

    codigo alterado

--- teste.js
+++ teste.js
@@ -1,14 +1,1 @@
-const n1 = 5
-const n2 = 10
-const soma = n1+n2
-console.log(soma);
-const n3 = 20
-sub = soma*n3
-console.log(sub);
-console.log("Aula de Git");
-class Carro{
-    constructor(tipo, capacidade){
-        this.tipo = tipo;
-        this.capacidae = capacidade
-    }
-}
+console.log("branch suporte");
