a5daca1e66f362700d03a3ec70f6449a66242a55
rwotter <renata.wotter@gmail.com>
2024-06-07 17:28:45 -0300

    classe animal

--- teste.js
+++ teste.js
@@ -1,1 +1,26 @@
-console.log("branch suporte");
+const n1 = 5
+const n2 = 10
+const soma = n1+n2
+console.log(soma);
+const n3 = 20
+sub = soma*n3
+console.log(sub);
+console.log("Aula de Git");
+class Carro{
+    constructor(tipo, capacidade){
+        this.tipo = tipo;
+        this.capacidae = capacidade
+    }
+   
+}
+
+const gol = new Carro("carro", 5);
+
+class Animal{
+    constructor(tio, patas){
+        this.tipo = tipo;
+        this.patas = patas;
+    }
+}
+
+const cachorro = new Animal("mamifero", 4);
